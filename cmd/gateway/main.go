ae17f758f284b5e31b83420549a5cf48de386e49
DavidDengHui <david.no.1@msn.cn>
2023-06-11 04:33:35 +0800

    update 1.32

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -18,13 +18,14 @@ func main() {
 	flag.Parse()
 
 	http.HandleFunc("/api/feed", feed)
+	http.HandleFunc("/api/bili", bili)
 	listener := gateway.ListenAndServe
 	portStr := "n/a"
 
 	if *port != -1 {
 		portStr = fmt.Sprintf(":%d", *port)
 		listener = http.ListenAndServe
-		http.Handle("/", http.FileServer(http.Dir("./public")))
+		http.Handle("/", http.FileServer(http.Dir("./static")))
 	}
 
 	log.Fatal(listener(portStr, nil))
@@ -32,6 +33,35 @@ func main() {
 
 func feed(w http.ResponseWriter, r *http.Request) {
 	url := "https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=50&desktop=true"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.Write(body)
+}
+
+func feed(w http.ResponseWriter, r *http.Request) {
+	url := "https://api.bilibili.com/x/web-interface/popular?ps=20&pn=1"
 	method := "GET"
 
 	client := &http.Client{}
