70cf2d342e595b8841864a6830bb53f27f7f418c
DavidDengHui <david.no.1@msn.cn>
2023-06-24 23:18:15 +0800

    v2.50

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -236,14 +236,14 @@ func DoitHandler(w http.ResponseWriter, r *http.Request) {
 						state := r.URL.Query().Get("state")
 						page_url := r.URL.Query().Get("url")
 						if r.Method == "POST" {
-							fmt.Fprintf(w, fmt.Sprintf("%s|%s[%s]->[%s]:[%s]", token, hookName, state, page_url, PayLoad))
-							return
 							if vaule, ok := PayLoad["deployment_status"].(map[string]interface{})["state"]; ok {
 								state = vaule.(string)
 							}
 							if vaule, ok := PayLoad["deployment_status"].(map[string]interface{})["environment_url"]; ok {
 								page_url = vaule.(string)
 							}
+							fmt.Fprintf(w, fmt.Sprintf("%s|%s[%s]->[%s]:[%s]", token, hookName, state, page_url, PayLoad))
+							return
 						}
 						if state == "success" {
 							status_data["status"] = "success"
